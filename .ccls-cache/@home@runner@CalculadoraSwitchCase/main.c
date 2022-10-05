04c204b85b6ca0712ccd90cc38093beff3d0e6a0
Sergio Gabriel <97525050+Sergio-Aquino@users.noreply.github.com>
2022-10-05 14:17:38 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@CalculadoraSwitchCase/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h> 
+
+int main(){
+ 
+ 	int opc = 0;
+ 	float num1 = 0;
+ 	float num2 = 0;
+ 	float res = 0;
+ 	
+ 	
+ 	do{
+      printf("0- Sair\n");
+ 		  printf("1- Soma\n");
+ 		  printf("2- Subtração\n");
+ 		  printf("3- Multiplicação\n");
+ 		  printf("4- Divisão\n");
+ 		  printf("5- Potência\n");
+ 		  printf("6- Mod\n");
+ 		  printf("7- Div\n");
+ 		  printf("8- MMC\n");
+ 		  printf("9- MDC\n");
+ 		  scanf("%d", &opc);
+ 		
+ 		  switch(opc){
+ 			  case 1:
+ 				  res = num1 + num2;
+ 				  break;
+ 			
+ 			  case 2:
+          res = num1 - num2;
+          break;
+ 			
+ 			  case 3:
+ 			
+ 			  case 4: 
+ 			
+ 			  case 5:
+ 			
+ 			  case 6: 
+ 			
+ 			  case 7:
+ 			
+ 			  case 8:
+ 			
+ 			  case 9: 
+ 
+  } while(opc != 0);
+    
+ 	 
+ 
+ 	return 0;
+}
