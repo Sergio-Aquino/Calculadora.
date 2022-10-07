3660ef61bbd50084fcac80b3bf0fad31a0a5d458
Sergio Gabriel <97525050+Sergio-Aquino@users.noreply.github.com>
2022-10-07 20:40:55 +0000

    Indentação arrumada.

--- .ccls-cache/@home@runner@CalculadoraSwitchCase/main.c
+++ .ccls-cache/@home@runner@CalculadoraSwitchCase/main.c
@@ -12,6 +12,7 @@ void mmc(float num3, float num4);
 void mdc(float num3, float num4);
 
 
+
 int main(){
   
  	int opc = 0;
@@ -39,57 +40,57 @@ int main(){
       } else if(opc < 0 || opc > 9){
           printf("\nOpção inválida. Tente novamente.\n");
         
-      }else{ 
-        printf("\nInforme um número: ");
-        scanf("%f", &num1);
+      } else { 
+          printf("\nInforme um número: ");
+          scanf("%f", &num1);
 
-        printf("\nAgora, informe outro número: ");
-        scanf("%f", &num2);
+          printf("\nAgora, informe outro número: ");
+          scanf("%f", &num2);
         
- 		    switch(opc){
-          case 1:
- 				    soma(num1, num2);
- 				    break;
+ 		      switch(opc){
+            case 1:
+ 				      soma(num1, num2);
+ 				      break;
  			
- 			    case 2:
-            subtracao(num1, num2);
-            break;
+ 			      case 2:
+              subtracao(num1, num2);
+              break;
  			
- 			    case 3:
-            multiplicacao(num1, num2);
-            break; 
+ 			      case 3:
+              multiplicacao(num1, num2);
+              break; 
  			
- 			    case 4:
-            divisao(num1, num2);
-            break; 
+ 			      case 4:
+              divisao(num1, num2);
+              break; 
  			
- 			    case 5:
-            potencia(num1, num2);
-            break; 
+ 			      case 5:
+              potencia(num1, num2);
+              break; 
  			
- 			    case 6:
-            mod(num1, num2);
-            break;
+ 			      case 6:
+              mod(num1, num2);
+              break;
  			
- 			    case 7:
-            div(num1, num2);
-            break;
+ 			      case 7:
+              div(num1, num2);
+              break;
  			
- 			    case 8:
-            mmc(num1, num2);
-            break;
+ 			      case 8:
+              mmc(num1, num2);
+              break;
  			
- 			    case 9:
-            mdc(num1, num2);
-            break;
+ 			      case 9:
+              mdc(num1, num2);
+              break;
+          }
         }
-      }
     
-  }while(opc != 0);
+    }while(opc != 0);
 
-  printf("\nPrograma encerrado com sucesso!"); 	
+    printf("\nPrograma encerrado com sucesso!"); 	
   
-  return 0;
+    return 0;
 }
 
 
